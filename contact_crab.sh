4119537221bbfd7b3538a3091c40802ea2c0019e
sOm2y <yyin888@aucklanduni.ac.nz>
2015-04-13 12:51:32 +1200

    init

--- contact_crab.sh
+++ contact_crab.sh
@@ -3,39 +3,74 @@
 #Company: Coachseek
 #Description: this is a bash script for Denym tracking customer contacts
 
-cd ~/Downloads
 
-#install wget
-curl -O http://ftp.gnu.org/gnu/wget/wget-1.15.tar.gz
-tar -zxvf wget-1.15.tar.gz
-cd wget-1.15/
-./configure
-./configure --with-ssl=openssl
-make
-sudo make install
-rm -rf ~/Downloads/wget*
+WGET_Path=wget
+DoesWGETExist=`$WGET_Path --version || echo "false"`
+if [ "$DoesWGETExist" != false ]; then
+  echo "==============================================================="
+  echo "Please enter URL you want to collect from, followed by [ENTER]:"
+  echo "==============================================================="
+  read URL
+  echo "========================================="
+  echo " Collecting information from given url..."
+  echo "========================================="
+  cd ~/crab_contacts
+  html="contact.html"
+  txt="contact.txt"
 
-#install lynx 
-cd /usr/local/src
-curl -O http://lynx.isc.org/lynx2.8.7/lynx2.8.7.tar.gz
-tar -xzvf lynx2.8.7.tar.gz
-cd lynx2-8-7
-./configure --mandir=/usr/share/man
-make
-sudo make install
-rm -rf ~/Downloads/lynx*
+  if [ -f "$html" ]
+  then
+    echo "$html found."
+    rm contact.html
+  fi 
+  if [ -f "$txt" ]
+  then
+    echo "$txt found."
+    rm contact.txt
+  fi
+  wget $URL -O contact.html
+  echo "========================================="
+  echo " $html has been created"
+  echo "========================================="
+  lynx --dump contact.html > contact.txt
+  echo "========================================="
+  echo " $txt has been created"
+  echo "========================================="
+
+  echo "========================================="
+  echo " Collecting Completed"
+  echo "========================================="
 
 
-cd ~/crab_contacts
+else
+  echo "========================================================"
+  echo " Missing dependency: wget (http://gnu.org/software/wget)"
+  echo " Installing..."
+  echo "========================================================"
+  cd ~/Downloads
+  #install wget
+  curl -O http://ftp.gnu.org/gnu/wget/wget-1.15.tar.gz
+  tar -zxvf wget-1.15.tar.gz
+  cd wget-1.15/
+  ./configure
+  ./configure --with-ssl=openssl
+  make
+  sudo make install
+  rm -rf ~/Downloads/wget*
+  #install lynx 
+  cd /usr/local/src
+  curl -O http://lynx.isc.org/lynx2.8.7/lynx2.8.7.tar.gz
+  tar -xzvf lynx2.8.7.tar.gz
+  cd lynx2-8-7
+  ./configure --mandir=/usr/share/man
+  make
+  sudo make install
+  rm -rf ~/Downloads/lynx*
+  exit
+fi
 
-#Remove old files
-rm contact.html
-rm contact.txt
-
-URL="http://www.atpca.com.au/coach-search/?w1=-33.8674869&w2=151.20699020000006&dist=15"
 
 
-wget $URL -O contact.html
-lynx --dump contact.html > contact.txt
+
 
 
